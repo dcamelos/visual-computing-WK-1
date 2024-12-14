3e6edb576bffbe7f703ab61dae052230b715bdce
sva-alt <175239487+sva-alt@users.noreply.github.com>
2024-12-13 19:55:03 -0500

    add ship's movement and screen edges

--- ship.js
+++ ship.js
@@ -1,26 +1,79 @@
+
+
 class Ship{
     constructor(){
         this.pos = createVector(windowWidth/2, windowHeight/2);
         this.r = 20;
+        this.heading = 0;
+        this.rotation = 0;
+        this.vel = createVector(0,0);
+        this.acc = createVector(0,0);
+        this.isBoosting = false;
+
    }
 
     show(){
-        this.shipShape(this.pos.x + 0, 
-                    this.pos.y - this.r, 
-                    this.pos.x - this.r * 0.65, 
-                    this.pos.y + this.r, 
-                    this.pos.x + this.r * 0.65, 
-                    this.pos.y + this.r, 
+        translate(this.pos.x, this.pos.y);
+        this.shape(0, 
+                    - this.r, 
+                    - this.r * 0.65, 
+                    + this.r, 
+                    + this.r * 0.65, 
+                    + this.r, 
                     this.r * 1.90, 
                     this.r * 0.75);
     }
 
     update(){
+        if (this.isBoosting) {
+            this.boost();
+        }
+        this.turn();
+        this.pos.add(this.vel);
+        this.vel.mult(0.97);
+        this.edges();
+    }
 
+    boost(){
+        var force = p5.Vector.fromAngle(this.heading);
+        force.mult(0.5);
+        this.vel.add(force);
+    }
+
+    boosting(b){
+        this.isBoosting = b;
+    }
+
+    turn(){
+        this.heading += this.rotation;
+    }
+
+    setRotation(angle){
+        this.rotation = angle;
+    }
+
+    edges(){
+        if (this.pos.x > width + this.r)
+        {
+            this.pos.x = - this.r;
+        }
+        else if (this.pos.x < -this.r){
+            this.pos.x = width + this.r;
+        }
+
+        if (this.pos.y > height+ this.r)
+        {
+            this.pos.y = - this.r;
+        }
+        else if (this.pos.y < -this.r){
+            this.pos.y = height + this.r;
+        }
     }
 
 
-    shipShape(x1, y1, x2, y2, x3, y3, w, h){
+
+
+    shape(x1, y1, x2, y2, x3, y3){
         /* Pinta la forma de nuestra nave
         
         Mirando hacia arriba, 
@@ -29,10 +82,11 @@ class Ship{
         x3 es el punto abajo a la derecha
         
         */
+
+        rotate(this.heading + PI/2);
         let arcCenter = createVector((x2 + x3)/2, (y2 + y3)/2);
         stroke(255);
         fill(255);
-        strokeWeight(2);
         noFill();
         line(x1, y1, x2, y2);
         line(x1, y1, x3, y3);
--- sketch.js
+++ sketch.js
@@ -10,3 +10,20 @@ function draw() {
   ship.show();
   ship.update();
 }
+
+function keyReleased() {
+    ship.setRotation(0);
+    ship.boosting(false)
+}
+
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW){
+      ship.setRotation(0.1);
+  }
+  if (keyCode == LEFT_ARROW){
+      ship.setRotation(-0.1);
+  }
+  if (keyCode == UP_ARROW){
+    ship.boosting(true);
+  }
+}
