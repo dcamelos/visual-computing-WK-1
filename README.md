4fa80e93e717c21d81539f1c915a146c9826c7c7
dcamelos <dcamelos@unal.edu.co>
2024-12-17 14:25:20 -0500

    bug fix and deploy

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # visual-computing-WK-1
 
 Replica del juego Asteroids, en p5.js.
+
+Enlace para jugar: https://dcamelos.github.io/visual-computing-WK-1/
--- sketch.js
+++ sketch.js
@@ -22,28 +22,8 @@ function draw() {
   ship.show();
   ship.update();
 
-  for (var i = lasers.length - 1 ; i  >= 0; i--){
-    lasers[i].render();
-    lasers[i].update();
-    for(var j =asteroids.length - 1; j >= 0; j--){
-      if (lasers[i].hits(asteroids[j])){
-        if (asteroids[j].r>20){
-          var newAsteroids = asteroids[j].breakup();
-          asteroids = asteroids.concat(newAsteroids);
-        }
-          asteroids.splice(j,1);
-          lasers.splice(i,1);
-          break;
-
-          }
-    }
-    if (lasers[i].offscreen()){
-      lasers.splice(i,1);
-    }
-  }
 
   for (var i = lasers.length - 1; i >= 0; i--){
-
     lasers[i].render();
     lasers[i].update();
     if (lasers[i] && lasers[i].offscreen()){
